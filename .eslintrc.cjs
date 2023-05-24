e0f6024e8c3b919089204fbbdcf970e3a91b499b
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-05-24 05:13:01 +0300

    Create files and configs for fresh setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'jest'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error'
+  }
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>It works and you found me!</h1>
+    </div>
+  );
+}
+
+export default App;
